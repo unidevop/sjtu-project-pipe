0b77585241862a4fed6b901253f6743f6d844044
useminmin@gmail.com <useminmin@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2011-08-23 13:04:17 +0000

    Add a new form to specify a detail time to view the scene of that scene.
    This form will be activated by clicking the view specific time button on the toolbar.
    
    And this button is disabled in replay running mode and vice visa.

---
+++ trunk/ sjtu-project-pipe/src/PipeSimulation/SpecifyTime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PipeSimulation
+{
+    public partial class SpecifyTime : Form
+    {
+        public SpecifyTime()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method is used to set the initial date time for date timer picker
+        /// </summary>
+        public DateTime InitialDateTime
+        {
+            set
+            {
+                dateTimePicker.Value = value;
+            }
+        }
+
+        /// <summary>
+        /// This method is used to get the user selected date time
+        /// </summary>
+        public DateTime SelectedDateTime
+        {
+            get
+            {
+                return dateTimePicker.Value;
+            }
+        }
+    }
+}
