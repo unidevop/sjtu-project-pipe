d3c6cd0fcb2b22daa057b2a4f848ebbfcb20d21e
joseph.woo.hust@gmail.com <joseph.woo.hust@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2011-08-30 17:02:49 +0000

    Revised code according to the latest interface.
    Need more changes tomorrow.

--- trunk/ sjtu-project-pipe/src/DataQuery/DataQueryManager.cs
+++ trunk/ sjtu-project-pipe/src/DataQuery/DataQueryManager.cs
@@ -7,24 +7,24 @@ namespace PipeSimulation.DataQuery
 {
     public class DataQueryManager
     {
-        //public static IDataQuery GetRealTimeQuery(string dbAdress, string dbName, string userName, string password, double interval)
-        //{
-        //    return new RealTimeDataQuery(dbAdress, dbName, userName, password, interval);
-        //}
+        public static IRealtimeDataQuery GetRealTimeQuery(string dbAdress, string dbName, string userName, string password, double interval)
+        {
+            return new RealTimeDataQuery(dbAdress, dbName, userName, password, interval);
+        }
 
-        //public static IDataQuery GetRealTimeQuery(string connString, double interval)
-        //{
-        //    return new RealTimeDataQuery(connString, interval);
-        //}
+        public static IRealtimeDataQuery GetRealTimeQuery(string connString, double interval)
+        {
+            return new RealTimeDataQuery(connString, interval);
+        }
 
-        //public static IDataQuery GetHistoricalQuery(string dbAdress, string dbName, string userName, string password, double interval)
-        //{
-        //    return new HistoricalDataQuery(dbAdress, dbName, userName, password, interval);
-        //}
+        public static IHistoryDataQuery GetHistoricalQuery(string dbAdress, string dbName, string userName, string password)
+        {
+            return new HistoricalDataQuery(dbAdress, dbName, userName, password);
+        }
 
-        //public static IDataQuery GetHistoricalQuery(string connString, double interval)
-        //{
-        //    return new HistoricalDataQuery(connString, interval);
-        //}
+        public static IHistoryDataQuery GetHistoricalQuery(string connString)
+        {
+            return new HistoricalDataQuery(connString);
+        }
     }
 }
