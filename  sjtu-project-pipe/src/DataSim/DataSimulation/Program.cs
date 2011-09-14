ac26bccc9f4a45c37c20b803245c87a18b1a6027
joseph.woo.hust <joseph.woo.hust@gmail.com>
2011-09-14 17:02:07 +0000

    data simulation config

---  sjtu-project-pipe/src/DataSim/DataSimulation/Program.cs
+++  sjtu-project-pipe/src/DataSim/DataSimulation/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Configuration;
+using System.Collections;
+using System.Windows.Media.Media3D;
 
 namespace DataSimulation
 {
@@ -16,8 +18,24 @@ namespace DataSimulation
             {
                 string connStr = ConfigurationManager.ConnectionStrings["SQLDBConn"].ConnectionString;
 
+                string sectionBaseName = "pipes/pipeSection";
+                Hashtable ht = null;
+                int idx = 1;
+
+                while ((ht = (Hashtable)(ConfigurationManager.GetSection(sectionBaseName + idx++))) != null)
+                {
+                    Point3D startPt1 = new Point3D(Convert.ToDouble(ht["startX1"]), Convert.ToDouble(ht["startY1"]), Convert.ToDouble(ht["startZ1"]));
+                    Point3D startPt2 = new Point3D(Convert.ToDouble(ht["startX2"]), Convert.ToDouble(ht["startY2"]), Convert.ToDouble(ht["startZ2"]));
+                    Point3D endPt1 = new Point3D(Convert.ToDouble(ht["endX1"]), Convert.ToDouble(ht["endY1"]), Convert.ToDouble(ht["endZ1"]));
+                    Point3D endPt2 = new Point3D(Convert.ToDouble(ht["endX2"]), Convert.ToDouble(ht["endY2"]), Convert.ToDouble(ht["endZ2"]));
+                }
+
                 dataSim = new PipeDataSim(connStr,
-                    Convert.ToDouble(ConfigurationManager.AppSettings["WriteInterval"]));
+                    TimeSpan.Parse(ConfigurationManager.AppSettings["WriteInterval"]),
+                    Convert.ToDateTime(ConfigurationManager.AppSettings["StartTime"]),
+                    Convert.ToDateTime(ConfigurationManager.AppSettings["EndTime"]),
+                    TimeSpan.Parse(ConfigurationManager.AppSettings["MeasureInterval"]),
+                    TimeSpan.Parse(ConfigurationManager.AppSettings["BackfillTimeSpan"]));
 
                 //Console.WriteLine("按任意键继续...");
                 Console.ReadLine();
