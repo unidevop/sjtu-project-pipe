4250e0681fedcf3704b952394ec9012651817ef0
useminmin@gmail.com <useminmin@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2011-09-21 12:27:50 +0000

    Update the missing files.

---
+++ trunk/ sjtu-project-pipe/src/PipeSimulation/AppFramework/RendererManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace PipeSimulation.PipeApp
+{
+    /// <summary>
+    /// IRendererManager acts a manager to provide the access point of all renderers
+    /// </summary>
+    public interface IRendererManager
+    {
+        vtk.vtkRenderer MainRenderer { get; }
+        vtk.vtkRenderer TopViewRenderer { get; }
+        vtk.vtkRenderer FrontViewRenderer { get; }
+        vtk.vtkRenderer RightViewRenderer { get; }
+
+        vtk.vtkRenderer ActiveRenderer { get; set; }
+        IRendererLayoutStrategy RendererLayoutStrategy { set; }
+    }
+
+    /// <summary>
+    /// Implementation to the IRendererManager
+    /// </summary>
+    public class CRendererManager : IRendererManager
+    {
+        private vtk.vtkRenderer m_MainRenderer = new vtk.vtkRenderer();
+        private vtk.vtkRenderer m_TopViewRenderer = new vtk.vtkRenderer();
+        private vtk.vtkRenderer m_FrontViewRenderer = new vtk.vtkRenderer();
+        private vtk.vtkRenderer m_RightViewRenderer = new vtk.vtkRenderer();
+        private vtk.vtkRenderer m_activeRender = null;
+        private IRendererLayoutStrategy m_RendererLayoutStrategy = null;
+
+        public CRendererManager()
+        {
+            ActiveRenderer = MainRenderer;
+        }
+
+        #region IRendererManager members
+
+        public vtk.vtkRenderer MainRenderer
+        {
+            get { return m_MainRenderer; }
+        }
+
+        public vtk.vtkRenderer TopViewRenderer
+        {
+            get { return m_TopViewRenderer; }
+        }
+
+        public vtk.vtkRenderer FrontViewRenderer
+        {
+            get { return m_FrontViewRenderer; }
+        }
+
+        public vtk.vtkRenderer RightViewRenderer
+        {
+            get { return m_RightViewRenderer; }
+        }
+
+        public vtk.vtkRenderer ActiveRenderer 
+        {
+            get
+            {
+                return m_activeRender;
+            }
+            set
+            {
+               m_activeRender = value;
+            }
+        }
+
+        public IRendererLayoutStrategy RendererLayoutStrategy
+        {
+            set 
+            {
+                m_RendererLayoutStrategy = value;
+                m_RendererLayoutStrategy.ApplyLayout();
+            }
+        }
+
+        #endregion
+    }
+}
