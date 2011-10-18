2b55d8ee5fd6a3bff6fd13d780c6fd94f3f97290
useminmin@gmail.com <useminmin@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2011-10-18 05:50:58 +0000

    Update Missing file for the new replay command.

---
+++ trunk/ sjtu-project-pipe/src/PipeSimulation/Commands/replaySimulationCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+using PipeSimulation.Utility;
+using PipeSimulation.Properties;
+using PipeSimulation.PipeApp;
+using PipeSimulation.ObserverMode;
+
+namespace PipeSimulation.Commands
+{
+    public class CReplaySimulationCommand : CCommand
+    {
+        public CReplaySimulationCommand(ICommandManager commandManager)
+            : base(commandManager, (ulong)CommandIds.kReplaySimulation)
+        {
+        }
+
+        protected override void OnActivate()
+        {
+            try
+            {
+                // The current observer mode instance must be CReplayMode
+                CReplayMode replayMode = IApp.theApp.ObserverModeManager.ActiveModeInstance as CReplayMode;
+                if (replayMode == null) return;
+
+                replayMode.ReplayAnimationEngine.StartAnimation();
+            }
+            catch (SystemException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        protected override void OnTerminate()
+        {
+            // The current observer mode instance must be CReplayMode
+            CReplayMode replayMode = IApp.theApp.ObserverModeManager.ActiveModeInstance as CReplayMode;
+            if (replayMode == null) return;
+
+            replayMode.ReplayAnimationEngine.StopAnimation();
+        }
+    }
+}
