9d87d562351990a7122e057d34c155ff9ea6ec77
useminmin <useminmin@gmail.com>
2011-11-10 12:48:12 +0000

    Update the application's title and about dialog.

---  sjtu-project-pipe/src/PipeSimulation/MainUI.Designer.cs
+++  sjtu-project-pipe/src/PipeSimulation/MainUI.Designer.cs
@@ -837,7 +837,7 @@ namespace PipeSimulation
             this.Controls.Add(this.statusStrip1);
             this.MainMenuStrip = this.menuStrip;
             this.Name = "MainUI";
-            this.Text = "天津市滨海新区中央大道海河隧道工程三维实时可视化系统";
+            this.Text = "天津市滨海新区中央大道海河隧道工程三维实时可视化监测系统";
             Bitmap appicationBmp = global::PipeSimulation.Properties.Resources.application;
             this.Icon = Icon.FromHandle(appicationBmp.GetHicon());
             this.splitContainerMain.Panel2.ResumeLayout(false);
