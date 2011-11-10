7bfd0e62b042498ccc5a906f393265d61deb4382
joseph.woo.hust@gmail.com <joseph.woo.hust@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2011-11-10 09:05:13 +0000

    Update test data generation app. the test data seems incorrect.

--- trunk/ sjtu-project-pipe/src/DataSim/DataSimulation/PipeConfig.cs
+++ trunk/ sjtu-project-pipe/src/DataSim/DataSimulation/PipeConfig.cs
@@ -116,7 +116,7 @@ namespace DataSimulation
 
         protected override object GetElementKey(ConfigurationElement element)
         {
-            return ((PipeElement)element).Id;
+            return ((PipeElement)element).Index;
         }
 
         public override ConfigurationElementCollectionType CollectionType
@@ -147,7 +147,21 @@ namespace DataSimulation
 
     internal class PipeElement : ConfigurationElementCollection
     {
-        [ConfigurationProperty("id", DefaultValue = "1", IsKey=true, IsRequired = true)]
+        [ConfigurationProperty("index", DefaultValue = "1", IsKey=true, IsRequired = true)]
+        [IntegerValidator(MinValue = 0, ExcludeRange = false)]
+        public int Index
+        {
+            get
+            {
+                return (int)base["index"];
+            }
+            set
+            {
+                base["index"] = value;
+            }
+        }
+
+        [ConfigurationProperty("id", DefaultValue = "1", IsRequired = true)]
         [IntegerValidator(MinValue = 0, MaxValue = 4, ExcludeRange = false)]
         public int Id
         {
@@ -158,6 +172,45 @@ namespace DataSimulation
             set
             {
                 base["id"] = value;
+            }
+        }
+
+        [ConfigurationProperty("StartTime")]
+        public DateTime StartTime
+        {
+            get
+            {
+                return (DateTime)base["StartTime"];
+            }
+            set
+            {
+                base["StartTime"] = value;
+            }
+        }
+
+        [ConfigurationProperty("EndTime")]
+        public DateTime EndTime
+        {
+            get
+            {
+                return (DateTime)base["EndTime"];
+            }
+            set
+            {
+                base["EndTime"] = value;
+            }
+        }
+
+        [ConfigurationProperty("BackfillTimeSpan")]
+        public TimeSpan BackfillTimeSpan
+        {
+            get
+            {
+                return (TimeSpan)base["BackfillTimeSpan"];
+            }
+            set
+            {
+                base["BackfillTimeSpan"] = value;
             }
         }
 
