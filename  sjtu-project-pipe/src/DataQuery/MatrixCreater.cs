3d0255d1e5739bbf20964972680630005312cad5
useminmin <useminmin@gmail.com>
2011-11-16 02:34:51 +0000

    Fix a serious matrix problem that can not drive the model to the real GPS position correctly.

---  sjtu-project-pipe/src/DataQuery/MatrixCreater.cs
+++  sjtu-project-pipe/src/DataQuery/MatrixCreater.cs
@@ -36,13 +36,9 @@ namespace PipeSimulation.DataQuery
 
             vecY.Normalize();
             vecZ.Normalize();
-            //return new Matrix3D(vecX.X, vecY.X, vecZ.X, m_startPt.X,
-            //                    vecX.Y, vecY.Y, vecZ.Y, m_startPt.Y,
-            //                    vecX.Z, vecY.Z, vecZ.Z, m_startPt.Z,
-            //                    0, 0, 0, 1.0);
-            return new Matrix3D(vecX.X, vecX.Y, vecX.Z, m_startPt.X,
-                                vecY.X, vecY.Y, vecY.Z, m_startPt.Y,
-                                vecZ.X, vecZ.Y, vecZ.Z, m_startPt.Z,
+            return new Matrix3D(vecX.X, vecY.X, vecZ.X, m_startPt.X,
+                                vecX.Y, vecY.Y, vecZ.Y, m_startPt.Y,
+                                vecX.Z, vecY.Z, vecZ.Z, m_startPt.Z,
                                 0, 0, 0, 1.0);
         }
 
