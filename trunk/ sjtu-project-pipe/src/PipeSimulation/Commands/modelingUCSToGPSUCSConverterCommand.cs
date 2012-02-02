4eda4fd878aec7d19d3c5762ab5c502d554fb2c6
useminmin@gmail.com <useminmin@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2012-02-02 12:41:09 +0000

    Add a new command to convert the GPS UCS coordinate to modeling UCS coordinate.

---
+++ trunk/ sjtu-project-pipe/src/PipeSimulation/Commands/modelingUCSToGPSUCSConverterCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using PipeSimulation.Commands;
+using PipeSimulation.PipeApp;
+using System.Windows.Forms;
+using PipeSimulation.Utility;
+using PipeSimulation.Properties;
+
+namespace PipeSimulation
+{
+    namespace Commands
+    {
+        public class CModelingUCSToGPSUCSConverterCmd : CCommand
+        {
+            public CModelingUCSToGPSUCSConverterCmd(ICommandManager commandManager)
+                : base(commandManager, (ulong)CommandIds.kModelingUCSToGPSUCSConverter)
+            {
+            }
+
+            protected override void OnActivate()
+            {
+                try
+                {
+                    // Step 1: Pop up the dialog
+                    ModelingUCS__GPSUCS_Converter dlg = new ModelingUCS__GPSUCS_Converter();
+                    dlg.ShowDialog();
+                }
+                catch (SystemException e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    Terminate();
+                }
+            }
+        }
+    }
+}
---
+++ trunk/ sjtu-project-pipe/src/PipeSimulation/ModelingUCS__GPSUCS_Converter.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace PipeSimulation
+{
+    partial class ModelingUCS__GPSUCS_Converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModelingUCS__GPSUCS_Converter));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.GPSZ = new System.Windows.Forms.TextBox();
+            this.GPSY = new System.Windows.Forms.TextBox();
+            this.GPSX = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ModelingZ = new System.Windows.Forms.TextBox();
+            this.ModelingX = new System.Windows.Forms.TextBox();
+            this.ModelingY = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AccessibleDescription = null;
+            this.groupBox1.AccessibleName = null;
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.BackgroundImage = null;
+            this.groupBox1.Controls.Add(this.GPSZ);
+            this.groupBox1.Controls.Add(this.GPSY);
+            this.groupBox1.Controls.Add(this.GPSX);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = null;
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // GPSZ
+            // 
+            this.GPSZ.AccessibleDescription = null;
+            this.GPSZ.AccessibleName = null;
+            resources.ApplyResources(this.GPSZ, "GPSZ");
+            this.GPSZ.BackgroundImage = null;
+            this.GPSZ.Font = null;
+            this.GPSZ.Name = "GPSZ";
+            this.GPSZ.TextChanged += new System.EventHandler(this.GPSZ_TextChanged);
+            // 
+            // GPSY
+            // 
+            this.GPSY.AccessibleDescription = null;
+            this.GPSY.AccessibleName = null;
+            resources.ApplyResources(this.GPSY, "GPSY");
+            this.GPSY.BackgroundImage = null;
+            this.GPSY.Font = null;
+            this.GPSY.Name = "GPSY";
+            this.GPSY.TextChanged += new System.EventHandler(this.GPSY_TextChanged);
+            // 
+            // GPSX
+            // 
+            this.GPSX.AccessibleDescription = null;
+            this.GPSX.AccessibleName = null;
+            resources.ApplyResources(this.GPSX, "GPSX");
+            this.GPSX.BackColor = System.Drawing.SystemColors.Window;
+            this.GPSX.BackgroundImage = null;
+            this.GPSX.Font = null;
+            this.GPSX.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.GPSX.Name = "GPSX";
+            this.GPSX.TextChanged += new System.EventHandler(this.GPSX_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AccessibleDescription = null;
+            this.label3.AccessibleName = null;
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Font = null;
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AccessibleDescription = null;
+            this.label2.AccessibleName = null;
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Font = null;
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AccessibleDescription = null;
+            this.label1.AccessibleName = null;
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Font = null;
+            this.label1.Name = "label1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AccessibleDescription = null;
+            this.groupBox2.AccessibleName = null;
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.BackgroundImage = null;
+            this.groupBox2.Controls.Add(this.ModelingZ);
+            this.groupBox2.Controls.Add(this.ModelingX);
+            this.groupBox2.Controls.Add(this.ModelingY);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Font = null;
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // ModelingZ
+            // 
+            this.ModelingZ.AccessibleDescription = null;
+            this.ModelingZ.AccessibleName = null;
+            resources.ApplyResources(this.ModelingZ, "ModelingZ");
+            this.ModelingZ.BackgroundImage = null;
+            this.ModelingZ.Font = null;
+            this.ModelingZ.Name = "ModelingZ";
+            this.ModelingZ.TextChanged += new System.EventHandler(this.ModelingZ_TextChanged);
+            // 
+            // ModelingX
+            // 
+            this.ModelingX.AccessibleDescription = null;
+            this.ModelingX.AccessibleName = null;
+            resources.ApplyResources(this.ModelingX, "ModelingX");
+            this.ModelingX.BackgroundImage = null;
+            this.ModelingX.Font = null;
+            this.ModelingX.Name = "ModelingX";
+            this.ModelingX.TextChanged += new System.EventHandler(this.ModelingX_TextChanged);
+            // 
+            // ModelingY
+            // 
+            this.ModelingY.AccessibleDescription = null;
+            this.ModelingY.AccessibleName = null;
+            resources.ApplyResources(this.ModelingY, "ModelingY");
+            this.ModelingY.BackgroundImage = null;
+            this.ModelingY.Font = null;
+            this.ModelingY.Name = "ModelingY";
+            this.ModelingY.TextChanged += new System.EventHandler(this.ModelingY_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AccessibleDescription = null;
+            this.label6.AccessibleName = null;
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Font = null;
+            this.label6.Name = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AccessibleDescription = null;
+            this.label5.AccessibleName = null;
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Font = null;
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AccessibleDescription = null;
+            this.label4.AccessibleName = null;
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Font = null;
+            this.label4.Name = "label4";
+            // 
+            // ModelingUCS__GPSUCS_Converter
+            // 
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = null;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ModelingUCS__GPSUCS_Converter";
+            this.Load += new System.EventHandler(this.ModelingUCS__GPSUCS_Converter_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox GPSZ;
+        private System.Windows.Forms.TextBox GPSY;
+        private System.Windows.Forms.TextBox GPSX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox ModelingZ;
+        private System.Windows.Forms.TextBox ModelingX;
+        private System.Windows.Forms.TextBox ModelingY;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
