6370a5afce258156d684e6aef8898041b4139b19
useminmin <useminmin@gmail.com>
2012-03-17 10:55:45 +0000

    Move the unexpected exception handler to EntryUI.

---  sjtu-project-pipe/src/PipeSimulation/EntryUI.cs
+++  sjtu-project-pipe/src/PipeSimulation/EntryUI.cs
@@ -38,6 +38,15 @@ namespace PipeSimulation
 
         private static void ThreadProc(object s)
         {
+            // Deal with the un handled exception and thread exception
+            // We need to tell the developers what's wrong happened to the program.
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(/*MSG0*/"zh-chs");
+
             MainUI mainFrm = new MainUI(s);
             Application.Idle += new EventHandler(mainFrm.OnIdle);
             Application.Run(mainFrm);
@@ -47,5 +56,40 @@ namespace PipeSimulation
         {
             this.Close();
         }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+
+                ;
+                MessageBox.Show(Resources.IDS_FATAL_ERROR_TEXT + "\r\n" + ex.Message + ex.StackTrace,
+                   Resources.IDS_FATAL_ERROR_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+
+        private static void Application_ThreadException
+           (object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            DialogResult result = DialogResult.Abort;
+            try
+            {
+                result = MessageBox.Show(Resources.IDS_FATAL_ERROR_TEXT + "\r\n" + e.Exception.Message
+                 + e.Exception.StackTrace, Resources.IDS_FATAL_ERROR_TITLE,
+                 MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                if (result == DialogResult.Abort)
+                {
+                    Application.Exit();
+                }
+            }
+        } 
     }
 }
