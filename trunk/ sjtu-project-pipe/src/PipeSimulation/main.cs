4ccfab4b941c6578b0e6a572d658a5380f4e8042
useminmin@gmail.com <useminmin@gmail.com@5f61a789-efa3-6384-bbc9-fcce9a5041d8>
2012-03-17 10:55:45 +0000

    Move the unexpected exception handler to EntryUI.

--- trunk/ sjtu-project-pipe/src/PipeSimulation/main.cs
+++ trunk/ sjtu-project-pipe/src/PipeSimulation/main.cs
@@ -13,52 +13,9 @@ namespace PipeSimulation
         [STAThread]
         static void Main()
         {
-            // Deal with the un handled exception and thread exception
-            // We need to tell the developers what's wrong happened to the program.
-            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
-            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
- 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
             System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(/*MSG0*/"zh-chs");
 
             Application.Run(new EntryUI());
         }
-
-        static void CurrentDomain_UnhandledException
-         (object sender, UnhandledExceptionEventArgs e)
-        {
-            try
-            {
-                Exception ex = (Exception)e.ExceptionObject;
-
-                ;
-                MessageBox.Show(Resources.IDS_FATAL_ERROR_TEXT + "\r\n" + ex.Message + ex.StackTrace,
-                   Resources.IDS_FATAL_ERROR_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Stop);
-            }
-            finally
-            {
-                Application.Exit();
-            }
-        }
-
-        public static void Application_ThreadException
-           (object sender, System.Threading.ThreadExceptionEventArgs e)
-        {
-            DialogResult result = DialogResult.Abort;
-            try
-            {
-                result = MessageBox.Show(Resources.IDS_FATAL_ERROR_TEXT + "\r\n" + e.Exception.Message
-                 + e.Exception.StackTrace, Resources.IDS_FATAL_ERROR_TITLE,
-                 MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
-            }
-            finally
-            {
-                if (result == DialogResult.Abort)
-                {
-                    Application.Exit();
-                }
-            }
-        } 
     }
 }
